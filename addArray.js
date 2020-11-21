88470a0f32ef31d728a927f918901f5a10936e46
Peter Oyebanji <peter@prob.ca>
2020-11-20 22:11:50 -0700

    total

--- addArray.js
+++ addArray.js
@@ -1,12 +1,18 @@
 const arrayToNumber = (array)=>{
+   let newNumber = 0
+   if (!array) {
+      newNumber = 0
+   } else {
       let number = ''
       array.forEach(element => {
          number += element
       });
       newNumber = Number(number)
+   }
       return newNumber
    } 
 
+   
 const addArray = (a,b)=>{
   totalArray = []
    total = arrayToNumber(a) + arrayToNumber(b)
@@ -15,8 +21,7 @@ const addArray = (a,b)=>{
       totalArray.push(-totalString.charAt(1))
       for (i=2; i < totalString.length; i++) {
          totalArray.push(+totalString.charAt(i))
-         
-   } 
+  } 
    }   else if (total>0) {
       for (i=0; i < totalString.length; i++) {
          totalArray.push(+totalString.charAt(i))
@@ -34,7 +39,7 @@ console.log(addArray([3,2,6,6],[-7,2,2,8]))
 console.log(addArray([3,2,9], [1,2])); // Expected output: [3,4,1]
 console.log(addArray([4,7,3], [1,2,3])); // Expected output: [5,9,6]
 console.log(addArray([1], [5,7,6])); // Expected output: [5,7,7]
-console.log(addArray([], [])); // Expected output: []
+console.log(addArray([])); // Expected output: []
 console.log(addArray([1], [])); // Expected output: [1]
 console.log(addArray([], [1])); // Expected output: [1]
 console.log(addArray([1,2], [-1])); // Expected output: [1,1]
